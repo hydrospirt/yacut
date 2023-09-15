654f493798ec8a25ad40169621e89276dc3e3045
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-15 20:30:36 +0300

    Add __init__ settings

---
+++ yacut/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from settings import Config, TEMPLATE_DIR
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from yacut import api_views, cli_commands, error_handlers, views
