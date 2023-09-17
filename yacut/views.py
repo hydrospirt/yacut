9479c07ceb8ed8ec50b1b2296f1307354fa118b8
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-17 17:59:50 +0300

    Add new func add_to_db and redirect url_view

--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-from flask import render_template, flash
+from flask import render_template, flash, redirect, url_for
 
 from yacut import app, db
 from yacut.forms import URLForm
@@ -20,20 +20,42 @@ def generate_url():
     return random_string
 
 
+def add_to_db(form, short):
+    url = URLMap(
+        original=form.original.data,
+        short=short
+    )
+    db.session.add(url)
+    db.session.commit()
+    return flash(url_for('url_view', short=url.short, _external=True))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLForm()
+    short = form.short.data
     if form.validate_on_submit():
-        short = form.short.data
         if short is None:
             short = generate_url()
+            flash('Ваша новая ссылка готова:')
+            add_to_db(form, short)
         if URLMap.query.filter_by(short=short).first():
-            flash('Такой адрес занят, для вас был сгенерирован другой адрес')
+            flash('Такой адрес занят, для вас был сгенерирован другой адрес:')
             short = generate_url()
-        url = URLMap(
-            original=form.original.data,
-            short=short
-        )
-        db.session.add(url)
-        db.session.commit()
+            add_to_db(form, short)
+        else:
+            url = URLMap(
+                original=form.original.data,
+                short=short
+            )
+            db.session.add(url)
+            db.session.commit()
+            flash('Ваша новая ссылка готова:')
+            add_to_db(form, short)
     return render_template('index.html', form=form)
+
+
+@app.route('/<string:short>', methods=['GET'])
+def url_view(short):
+    url = URLMap.query.filter_by(short=short).first_or_404().original
+    return redirect(url)
