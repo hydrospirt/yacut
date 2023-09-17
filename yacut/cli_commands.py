ce6b3ceb6a046e8d8f5a6e307e7112a20c644c02
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-17 18:59:38 +0300

    Add cli load_links

---
+++ yacut/cli_commands.py
@@ -0,0 +1,36 @@
+import csv
+
+import click
+
+from yacut import app, db
+from yacut.models import URLMap
+from yacut.utils import generate_url
+
+
+def load_to_database(row, counter):
+    """Функция записи в базу данных, возвращает количество записанных обьектов"""
+    url = URLMap(**row)
+    db.session.add(url)
+    db.session.commit()
+    counter += 1
+    return counter
+
+
+@app.cli.command('load_links')
+def load_links_command():
+    """Функция загрузки ссылок в базу данных."""
+    with open('links.csv', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            if row['short'] is None:
+                row['short'] = generate_url()
+            if URLMap.query.filter_by(short=row['short']).first():
+                row['short'] = generate_url()
+                counter = load_to_database(row, counter)
+            else:
+                url = URLMap(**row)
+                db.session.add(url)
+                db.session.commit()
+                counter = load_to_database(row, counter)
+    click.echo(f'Загружено ссылок в базу данных: {counter}')
