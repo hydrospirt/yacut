edba0f8ec0df3c28743d3e7a32ea1d369c5f2aaf
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-17 20:04:30 +0300

    Add InvalidAPIUsage in get_short_url

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 from urllib.parse import urljoin
+
 from flask import jsonify, request
 
+from yacut import app, db
+from yacut.error_handlers import InvalidAPIUsage
 from yacut.models import URLMap
 from yacut.utils import generate_url
-
-from yacut import app, db
 
 
 @app.route('/api/id/<int:id>/', methods=['GET'])
@@ -13,5 +14,5 @@ def get_short_url(id):
     BASE_URL = request.url
     url = URLMap.query.get(id)
     if url is None:
-        pass
+        raise InvalidAPIUsage('Указанный id не найден', 404)
     return jsonify({'url': urljoin(BASE_URL, url.to_dict()['short'])}), HTTPStatus.OK
