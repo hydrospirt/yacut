5b711eaaf58dde180dbcae0a8c1420137f73e36f
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-17 20:02:37 +0300

    Add invalid_api_usage func

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import jsonify, render_template
+
 from yacut import app, db
 
 
@@ -13,6 +14,11 @@ class InvalidAPIUsage(Exception):
 
     def to_dict(self):
         return dict(message=self.message)
+
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(error):
+    return jsonify(error.to_dict()), error.status_code
 
 
 @app.errorhandler(404)
