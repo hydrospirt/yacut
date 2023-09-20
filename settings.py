c6a2bb48d29e59465b6d8be83c57b10abd60102a
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-20 20:23:54 +0300

    Add default settings

--- settings.py
+++ settings.py
@@ -6,7 +6,9 @@ STATIC_DIR = os.path.join(APP_PATH, 'html/static')
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                        default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY',
+                           default='Зачем тебе секретный ключ?')
     JSON_AS_ASCII = False
