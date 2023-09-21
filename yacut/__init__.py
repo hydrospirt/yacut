af9177cf83c31d6db49d40f7b8a15ff924c90363
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-21 16:32:12 +0300

    Replace settings.py

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from settings import Config, TEMPLATE_DIR, STATIC_DIR
+from yacut.settings import Config, TEMPLATE_DIR, STATIC_DIR
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.config.from_object(Config)
