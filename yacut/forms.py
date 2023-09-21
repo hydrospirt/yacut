9815226c4c8dc8e2380e6cfd317814e6df420b52
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-21 16:57:51 +0300

    Delete comment and Length validator

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,18 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import URL, DataRequired, Length, Optional, Regexp
+from wtforms.validators import URL, DataRequired, Optional, Regexp
 
 
 class URLForm(FlaskForm):
-    # Extremely long URLs are usually a mistake.
-    # URLs over 2,000 characters will not work in the most popular web browsers.
-    # Don't use them if you intend your site to work for the majority of Internet users.
     original = URLField(
         'Введите оригинальную ссылку',
         name='original_link',
         validators=(
             DataRequired(message='Обязательное поле'),
-            Length(1, 2048),
             URL(require_tld=False, message='URL-адрес содержит ошибки')
         ))
     short = StringField(
