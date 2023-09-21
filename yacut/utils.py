09a9176b42ef63718fb1770f0e8cf24b2e868c4c
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-21 17:38:11 +0300

    Add constants

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,18 +1,13 @@
 import random
-import re
-import string
 
 from yacut.models import URLMap
+from yacut.settings import GEN_LENGHT, GEN_SYMBOLS, MAXLEN_CUSTOM_ID, PATTERN
 
 
 def generate_url():
     """Генерирует строку из шести случайных символов [a-z],[A-Z],[0-9]"""
-    GEN_LENGHT = 6
-    random_string = ''.join(random.choices(
-        string.ascii_uppercase +
-        string.ascii_lowercase +
-        string.digits, k=GEN_LENGHT
-    ))
+    random_string = ''.join(
+        random.choices(GEN_SYMBOLS, k=GEN_LENGHT))
     return random_string
 
 
@@ -20,7 +15,6 @@ def is_already_in_database(short):
     """Проверяет запись в БД на уникальность значения"""
     if obj := URLMap.query.filter_by(short=short).first():
         return obj.short
-    return False
 
 
 def is_regex_custom_id_link(short):
@@ -30,7 +24,4 @@ def is_regex_custom_id_link(short):
         - цифры в диапазоне от 0 до 9;
         - Не больше 16 символов;
     """
-    pattern = re.compile('[a-zA-Z0-9]*$')
-    if pattern.match(short) and len(short) <= 16:
-        return False
-    return True
+    return PATTERN.match(short) and len(short) >= MAXLEN_CUSTOM_ID
