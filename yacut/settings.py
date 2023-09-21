8704f72d5d351c35730522cf8e989f3c04f76343
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-21 17:14:09 +0300

    Add constants

--- yacut/settings.py
+++ yacut/settings.py
@@ -1,8 +1,16 @@
 import os
+import re
+import string
 
 APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(APP_PATH, 'html/')
 STATIC_DIR = os.path.join(APP_PATH, 'html/static')
+GEN_SYMBOLS = (string.ascii_uppercase +
+               string.ascii_lowercase +
+               string.digits)
+GEN_LENGHT = 6
+PATTERN = re.compile('[a-zA-Z0-9]*$')
+MAXLEN_CUSTOM_ID = 16
 
 
 class Config(object):
@@ -10,5 +18,5 @@ class Config(object):
                                         default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY',
-                           default='Зачем тебе секретный ключ?')
+                           default='QAZwsx123')
     JSON_AS_ASCII = False
