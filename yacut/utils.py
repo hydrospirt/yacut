997ec5444f701b137baea230cd2f336d299a21b6
Эдуард Гумен <hydrospirt@yandex.ru>
2023-09-21 17:45:11 +0300

    Add annotations

--- yacut/utils.py
+++ yacut/utils.py
@@ -4,24 +4,22 @@ from yacut.models import URLMap
 from yacut.settings import GEN_LENGHT, GEN_SYMBOLS, MAXLEN_CUSTOM_ID, PATTERN
 
 
-def generate_url():
+def generate_url() -> str:
     """Генерирует строку из шести случайных символов [a-z],[A-Z],[0-9]"""
-    random_string = ''.join(
-        random.choices(GEN_SYMBOLS, k=GEN_LENGHT))
-    return random_string
+    return ''.join(random.choices(GEN_SYMBOLS, k=GEN_LENGHT))
 
 
-def is_already_in_database(short):
+def is_already_in_database(short: str) -> str:
     """Проверяет запись в БД на уникальность значения"""
     if obj := URLMap.query.filter_by(short=short).first():
         return obj.short
 
 
-def is_regex_custom_id_link(short):
+def is_regex_custom_id_link(short: str) -> bool:
     """ Проверяет на соотвествие паттерну:
         - большие латинские буквы;
         - маленькие латинские буквы;
         - цифры в диапазоне от 0 до 9;
         - Не больше 16 символов;
     """
-    return PATTERN.match(short) and len(short) >= MAXLEN_CUSTOM_ID
+    return PATTERN.match(short) and len(short) > MAXLEN_CUSTOM_ID
